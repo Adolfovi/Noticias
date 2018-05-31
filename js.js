8326e34b83cb0350b4ba5f4d0b81b3c4f1d3c6f3
Adolfovi <32370254+Adolfovi@users.noreply.github.com>
2018-05-31 19:37:55 +0200

    Update js.js

--- js.js
+++ js.js
@@ -78,7 +78,7 @@ $(window).scroll(function () {
                 //loading div show
                 $("#loading").fadeIn();
                 //carga json
-                $.getJSON("https://rawgit.com/IsmaFuentes/P-gina-web/master/json/" + numjson + ".json", function (jsonObject) {
+                $.getJSON("https://rawgit.com/" + numjson + ".json", function (jsonObject) {
                     buildrow(jsonObject);
                     //loading div hide
                     $("#loading").fadeOut();
